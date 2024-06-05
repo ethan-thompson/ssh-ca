ddbd14d2e9ef58859f47d355e1e5cd94c7783711
ethan-thompson <ethan.thompson@networkradius.com>
2024-06-05 16:53:01 -0400

    I have started working on the get_ssh_keys() function. I will need to get a list of loaded keys, and a list of keys in the .ssh file (using fingerprints) and see what is available for use

--- client-side/utils.py
+++ client-side/utils.py
@@ -2,10 +2,39 @@
 Utilities for handling keys and certificates
 """
 
+import subprocess
+
 """
-Get existing ssh keys if they exist, otherwise return 0
+Get existing ssh keys if they exist, otherwise generate keys. If successful, this will return the filename of a loaded key
 """
 def get_ssh_keys():
+    # Check if any keys are currently loaded in ssh
+    # Throw this in a try in case no matching keys exist for this host
+    try:
+        key_list = subprocess.check_output('ssh-add -l | grep -e "$(whoami)@$(hostname)"', shell=True, universal_newlines=True).split('\n')
+
+        # Remove the extra empty entry in the list (from when we split with newline)
+        key_list = key_list[0:len(key_list) - 1]
+
+        # Get a list of available keys in the .ssh folder
+
+        # Iterate through the list and see which keys are associated with which files in the users .ssh file
+
+        # Pick a key, and return the filename
+
+    except Exception as e:
+        # Check if the exception was raised because there were no keys
+        if ("returned non-zero exit status 1." in e):
+            # No keys exist, so let's generate some
+            return gen_ssh_keys()
+        
+        # A different error occured
+        print("Error: ", e)
+    
+    # ssh-keygen -l -f ~/.ssh/id_ecdsa.pub
+    subprocess.check_output('ssh-keygen -l -f ~/.ssh/id_ecdsa.pub', shell=True, universal_newlines=True).split('\n')
+
+
     return 0
 
 """
